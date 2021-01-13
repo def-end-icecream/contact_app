3d66f01424b629402b2fbe0064428c0a1ab7e6b3
dani <dani.zaghian@gmail.com>
2021-01-13 10:28:35 -0800

    adds seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+contact = Contact.new(first_name: "Casee", last_name: "Amores", email: "casee@gmail.com", phone_number: "444-5555")
+contact.save
+
+contact = Contact.new(first_name: "Mackenzie", last_name: "Evans", email: "mackenzie@gmail.com", phone_number: "555-6677")
+contact.save
+
+contact = Contact.new(first_name: "Theo", last_name: "Kim", email: "theo@gmail.com", phone_number: "444-8888")
+contact.save
+
+# Contact.create(
+#   [
+#     {
+#       first_name: "Casee", 
+#       last_name: "Amores", 
+#       email: "casee@gmail.com", 
+#       phone_number: "444-5555"
+#     },
+#     {
+#       first_name: "Mackenzie", 
+#       last_name: "Evans", 
+#       email: "mackenzie@gmail.com", 
+#       phone_number: "555-6677"
+#     },
+#     {
+#       first_name: "Theo", 
+#       last_name: "Kim", 
+#       email: "theo@gmail.com", 
+#       phone_number: "444-8888"
+#     }
+#   ]
+# )
