00c3e7a70070c2b4078e0a201d02d1fe33d34f06
dani <dani.zaghian@gmail.com>
2021-01-13 10:00:08 -0800

    adds single contact and all contacts routes

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -1,2 +1,13 @@
 class Api::ContactsController < ApplicationController
+
+  def single_contact_action
+    @contact = Contact.first
+    render "single_contact.json.jb"
+  end
+
+  def all_contacts_action
+    @contacts = Contact.all
+    render "all_contacts.json.jb"
+  end
+
 end
